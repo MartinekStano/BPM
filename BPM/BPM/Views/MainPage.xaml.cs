9125b6344bf38c742b5a6bad1aeb01daadb43855
Martin Staňo <martinmatostano@gmail.com>
2021-01-26 17:23:03 +0100

    Refactor to MVVM

---
+++ BPM/BPM/Views/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace BPM
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+            
+
+       
+}
